7b84de3132e93e52781b8528cce76790bf6efe18
chenyou <chenyouhuang@gmail.com>
2022-08-09 11:13:31 -0400

    Add airbnb linter back

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,12 @@
 // Bring React in to build a component.
-import React from "react";
+import React from 'react';
 // Bring reactDOM in to mount component to the dom.
-import reactDOM from "react-dom";
+import reactDOM from 'react-dom';
 
 // Here is out base App component.
 // Notice we are NOT using jsx here. This is because we have not set up babel yet.
-const App = React.createElement("h1", null, "Hello World");
+const App = React.createElement('h1', null, 'Hello World');
 
 // Render our app to the dom mounted to the element with id of root inside our
 // public/index.html file.
-reactDOM.render(App, document.getElementById("root"));
+reactDOM.render(App, document.getElementById('root'));
