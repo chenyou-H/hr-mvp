8364caf1f42c82720edf640ea448f78668db2e9f
chenyou <chenyouhuang@gmail.com>
2022-08-09 10:54:08 -0400

    Setting front end

---
+++ client/src/index.jsx
@@ -0,0 +1,11 @@
+// Bring React in to build a component.
+import React from "react";
+// Bring reactDOM in to mount component to the dom.
+import reactDOM from "react-dom";
+
+// Here is out base App component.
+// Notice we are NOT using jsx here. This is because we have not set up babel yet.
+const App = React.createElement("h1", null, "Hello World");
+
+// Render our app to the dom mounted to the element with id of root inside our public/index.html file.
+reactDOM.render(App, document.getElementById("root"));
